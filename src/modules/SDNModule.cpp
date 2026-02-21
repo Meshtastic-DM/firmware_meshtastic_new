36eb6da7c0d2c16dae340f26097de41702a53534
thisara-g <thisara200306@gmail.com>
2026-02-21 14:11:32 +0530

    SDN Announcement interval update

--- src/modules/SDNModule.cpp
+++ src/modules/SDNModule.cpp
@@ -109,7 +109,7 @@ SDNModule::SDNModule()
 
     // Test-only configuration until SDN config is added to module/local config protobufs.
     static constexpr uint32_t kTestControllerNode = 0x00000010;
-    static constexpr uint32_t kTestAnnouncementIntervalSec = 60;
+    static constexpr uint32_t kTestAnnouncementIntervalSec = 900;  // 15 minutes
     static constexpr const char *kTestSecret = "meshtastic-sdn-secret";
 
     announcementInterval = kTestAnnouncementIntervalSec;
